531482c81d36fa421d29579b17cb6510609fa7e6
Eswar Reddy <eswarreddy7999@gmail.com>
2022-12-03 00:45:36 +0530

    ver1

---
+++ src/App.js
@@ -0,0 +1,77 @@
+import './App.css';
+import axios from 'axios';
+import {useState} from 'react'
+
+
+function App() {
+  const [data1,setdata1]=useState([])
+  axios.get("https://carpsapi-production.up.railway.app/showdb").then((response)=>{
+    setdata1(response.data)
+  }).catch((error)=>{
+    console.log(error);
+  })
+  console.log(data1);
+  return (
+    <>
+    <div className="header">
+      <h1>Car Parking System</h1>
+    </div>
+    <div className="free-slots">
+      <h1>Free Slots</h1>
+        <div className="freeslotsflex">
+          
+        {data1.map((datax,e)=>{
+          return(
+            data1[e].status===0 &&
+            <>
+            <div className="freeslotsflex1"><h3>{data1[e].slot_id}</h3></div>
+            </>
+              
+            
+            
+          )
+          
+        })}
+     </div>
+    </div>
+    <div className='roadmap1'>
+      <h1>Slot Map</h1>
+      <div className='roadmap'>
+      {data1.map((datax,e)=>{
+          return(
+            
+            
+              <>
+              <div className='carslot'>
+              <div className='whitestrip'></div>
+              {data1[e].status===1 &&
+          <img src='https://www.seekpng.com/png/full/257-2576097_car-red-vehicle-automobile-auto-car-clipart-top.png' alt=''/>}
+          {data1[e].status===0 &&
+          <div className='blank'><h1>{data1[e].slot_id}</h1></div>}
+          <div className='whitestrip'></div>
+        </div>
+              </>
+              
+             
+            
+          )
+          
+        })}
+        
+        
+        
+        
+        
+      </div>
+      
+
+    </div>
+    <div className='footer'>
+      
+    </div>
+
+    </>
+  );
+}
+
+export default App;
